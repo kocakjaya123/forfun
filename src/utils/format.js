b600955740649800ba9c9e094b51d3bbc5122bc6
kocakjaya123 <ardhiseptiand@gmail.com>
2026-05-21 23:12:55 +0700

    initial commit - fresh start

--- src/utils/format.js
+++
@@ -1,7 +0,0 @@
-export function formatRupiah(value, { maximumFractionDigits = 0 } = {}) {
-  const n = Number(value) || 0;
-  const formatted = new Intl.NumberFormat('id-ID', { maximumFractionDigits }).format(n);
-  return `Rp ${formatted}`;
-}
-
-export default formatRupiah;
